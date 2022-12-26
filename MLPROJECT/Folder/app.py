5d0a6b6d471724c2e8acc0ffc9e8895ad5c407ab
NarasimhanKovalai <106007395+NarasimhanKovalai@users.noreply.github.com>
2022-12-27 02:20:08 +0530

    Add files via upload

---
+++ MLPROJECT/Folder/app.py
@@ -0,0 +1,48 @@
+import uvicorn
+from class_for_testing import AirlineFeedback
+from fastapi import FastAPI
+import pickle
+from main import prediction
+
+app=FastAPI()
+pickle_in=open("sentiment_analysis_model.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+
+#base endpoint which is the simplest API Endpoint
+@app.get('/')
+
+def index():
+    return{'message': 'Hello, there'}
+
+#endpoint to greet users
+@app.get('/Welcome')
+def get_name(name:str):
+    return{'Welcome to my website':f'{name}'}
+
+#endpoint which takes a text as input and outputs sentiment associated with it (positive or negative)
+@app.post('/predict')
+def predict_sentiment(data :AirlineFeedback):
+    data=data.dict()
+    print(data)
+    text=data['text']
+    prediction=classifier._test_single_feedback(text)
+    if (prediction[0]==0):
+        ans='negative'
+    else:
+        ans='positive'
+    
+    #return dictionary in JSON format - key value pair
+    return {
+      
+        'Sentiment':ans 
+    }
+    
+#run the main instance in local host
+if __name__=='__main__':
+    uvicorn.run(app,host='127.0.0.1',port=8010)
+
+
+
+
+
